df33dc1b237d5e2eed5ffb0e332006e67760ff6f
ShiraoTakuya <shirao_genu@yahoo.co.jp>
2020-10-28 20:23:43 +0900

    To seach all files

--- VideoPlayerWithResumefunction/index.php
+++ VideoPlayerWithResumefunction/index.php
@@ -5,24 +5,32 @@
 <title>動画再生、時間記録機能付き</title>
 </head>
 <body>
-<video id="video" width="400" height="300" src="./video/sample.mp4" controls></video>
-
-<script>
-var media = document.getElementById("video");
-media.currentTime = <?php 
-	$fname = "./data.log";
-	$fp = fopen($fname, "r");							// ファイルを開く
-	$buf = fread($fp, filesize($fname));	// 開いたファイルからデータを読み出す
-	fclose($fp);													// ファイルを閉じる
-	echo $buf;
+<?php
+	$files = glob('./video/*');
+	foreach($files as $file){
+		echo '<video id="'.$file.'" width="400" height="300" src="'.$file.'" controls></video>'."\r\n";
+	}
 ?>
 
-var timesend = function(){
-	var request = new XMLHttpRequest();
-	request.open('GET', './log.php?time='+String(media.currentTime), true);
-	request.send();
-} 
-setInterval(timesend, 1000);
+<script>
+	<?php
+		foreach($files as $file){
+			echo 'var media = document.getElementById("'.$file.'");'."\r\n";
+			echo 'media.currentTime = ';
+			$fname = "./data.log";
+			$fp = fopen($fname, "r");							// ファイルを開く
+			$buf = fread($fp, filesize($fname));	// 開いたファイルからデータを読み出す
+			fclose($fp);													// ファイルを閉じる
+			echo $buf;
+		}
+	?>
+
+	var timesend = function(){
+		var request = new XMLHttpRequest();
+		request.open('GET', './log.php?time='+String(media.currentTime), true);
+		request.send();
+	} 
+	setInterval(timesend, 1000);
 </script>
 
 </body> 
